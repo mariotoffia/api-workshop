fe3ac41b98894ba2a905a92925c5155579af5b94
Mario Toffia <mario.toffia@dataductus.se>
2016-04-28 08:39:06 +0200

    Show how GitHub Markdown docs can look like

--- Contest.Api/IContestService.cs
+++ Contest.Api/IContestService.cs
@@ -10,9 +10,10 @@ namespace Contest.Api
   {
     #region Players
 
-    [SwaggerWcfPath("Create player",
+    [SwaggerWcfPath("Create Player",
       "Creates a **Player** that can participate in contests. This player resides in " +
-      "the registry and will be copied each time a add player into a contest is invoked. " +
+      "the registry and will be copied each time a add player into a contest is invoked.\r\n" +
+      "## Throttling\r\n" +
       "This method is throttled and thus if **HTTP** response code 429 is returned, please try again " +
       "When the player is created this limit is described in the **X-Rate-Limit-Limit** header.")]
     [WebInvoke(UriTemplate = "/players", BodyStyle = WebMessageBodyStyle.Bare,
@@ -21,33 +22,35 @@ namespace Contest.Api
     Player CreatePlayer(
       [SwaggerWcfParameter(true, "Player to be created, the id will be set by server")] Player player);
 
-    [SwaggerWcfPath("Get players", "Retrieve all players")]
+    [SwaggerWcfPath("Get Players", "Retrieve all players")]
     [WebGet(UriTemplate = "/players", BodyStyle = WebMessageBodyStyle.Bare)]
     [OperationContract]
     Player[] GetPlayers();
 
-    [SwaggerWcfPath("Get players", "Retrieve a single player by using its player id")]
+    [SwaggerWcfPath("Get Players", "Retrieve a single player by using its player id")]
     [WebGet(UriTemplate = "/players/{id}", BodyStyle = WebMessageBodyStyle.Bare,
       RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
     [OperationContract]
     Player GetPlayerById([SwaggerWcfParameter(true, "Id of the player to get")] string id);
 
-    [SwaggerWcfPath("Update player", "Update a player")]
+    [SwaggerWcfPath("Update Player", "Update a player")]
     [WebInvoke(UriTemplate = "/players/{id}", BodyStyle = WebMessageBodyStyle.Bare, Method = "PUT",
       RequestFormat = WebMessageFormat.Json)]
     [OperationContract]
     Player UpdatePlayer(
       [SwaggerWcfParameter(true, "Player to be updated, note that the id must be properly set")] Player player);
 
-    [OperationContract]
+    [SwaggerWcfPath("Delete Player", "Permanently delete a player. However if he or she has participated " +
+      "in one or more contest, those are persisted. This is delete from the registry of players.")]
     [WebInvoke(UriTemplate = "/players/{id}", BodyStyle = WebMessageBodyStyle.Bare, Method = "DELETE")]
+    [OperationContract]
     void DeletePlayer([SwaggerWcfParameter(true, "Id of the player to delete")] string id);
 
     #endregion Players
 
     #region Contest
 
-    [SwaggerWcfPath("Create constest", "Creates a contest that players can participate in")]
+    [SwaggerWcfPath("Create Constest", "Creates a contest that players can participate in")]
     [WebInvoke(UriTemplate = "/contests", BodyStyle = WebMessageBodyStyle.Bare,
       RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
     [OperationContract]
