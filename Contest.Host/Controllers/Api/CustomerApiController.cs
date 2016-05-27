407f065ee0b7ab75decf3f8279bd8d844471902b
Mario Toffia <mario.toffia@dataductus.se>
2016-05-27 08:03:07 +0200

    Prettier swagger output for webapi

--- Contest.Host/Controllers/Api/CustomerApiController.cs
+++ Contest.Host/Controllers/Api/CustomerApiController.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Web.Http;
-using System.Web.Http.Results;
 using Contest.Host.Db;
 using Contest.Host.Model;
 using Marten;
 using NSwag.Annotations;
-using NSwag.CodeGeneration.SwaggerGenerators.WebApi;
 
 namespace Contest.Host.Controllers.Api
 {
   [RoutePrefix("api/customer")]
+  [Description("Customer Id Service.")]
   public sealed class CustomerApiController : ApiController
   {
     private readonly IDocumentSession _session;
@@ -29,6 +26,7 @@ namespace Contest.Host.Controllers.Api
     [Route("")]
     [ResponseType("200", typeof(Kund))]
     [Description("Gets all customers")]
+    [ActionName("GetAllCustomers")]
     public IEnumerable<Kund> Get()
     {
       return _session.Query<Kund>().ToArray();
@@ -37,53 +35,34 @@ namespace Contest.Host.Controllers.Api
     [HttpGet]
     [Route("{customer}")]
     [ResponseType("200", typeof(Kund))]
-    [ResponseType("500", typeof(ArgumentException))]
+    [ResponseType("404", typeof(string))]
     [Description("Gets a customer based on a code")]
-    public Kund Get(string customer)
+    public HttpResponseMessage Get(string customer)
     {
       var info = _session.Query<Kund>().SingleOrDefault(x => x.Kod == customer);
       if (null == info)
       {
-        throw new ArgumentException($"Customer with Kod = {customer} cannot be found.");
+        return Request.CreateResponse(HttpStatusCode.NotFound, $"Could not find Kund based on customer = {customer}");
       }
 
-      return info;
+      return Request.CreateResponse(HttpStatusCode.OK, info);
     }
 
     [HttpGet]
     [Route("{customer}/nr/{customerId}")]
-    [ResponseType("200", typeof(Kund))]
-    [ResponseType("500", typeof(ArgumentException))]
+    [ResponseType("200", typeof(Kundnummer))]
+    [ResponseType("404", typeof(string))]
     [Description("Gets a customer number based on a code and a customer id")]
-    public Kundnummer GetCustomerNumber(string customer, string customerId)
+    public HttpResponseMessage GetCustomerNumber(string customer, string customerId)
     {
       var info = _session.Query<Kundnummer>().SingleOrDefault(x => x.Kod == customer && x.Kund == customerId);
       if (null == info)
       {
-        throw new ArgumentException($"Customer number with Kod = {customer} and Kund = {customerId} cannot be found.");
+        return Request.CreateResponse(HttpStatusCode.NotFound,
+          $"Could not find Kundnummer based on customer = {customer} and customerId = {customerId}");
       }
 
-      return info;
-    }
-
-    private static readonly Lazy<string> SwaggerContent = new Lazy<string>(() =>
-    {
-      var settings = new WebApiToSwaggerGeneratorSettings
-      {
-        DefaultUrlTemplate = "api/{controller}/{action}/{id}"
-      };
-
-      var generator = new WebApiToSwaggerGenerator(settings);
-      var service = generator.GenerateForController(typeof(CustomerApiController), excludedMethodName: "Swagger");
-      return service.ToJson();
-    });
-
-    [HttpGet, Route("swagger")]
-    public HttpResponseMessage Swagger()
-    {
-      var res = Request.CreateResponse(HttpStatusCode.OK);
-      res.Content = new StringContent(SwaggerContent.Value, Encoding.UTF8, "application/json");
-      return res;
+      return Request.CreateResponse(HttpStatusCode.OK, info);
     }
 
     protected override void Dispose(bool disposing)
---
+++ Contest.Host/Controllers/Api/SwaggerApiController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using NSwag;
+using NSwag.CodeGeneration.SwaggerGenerators.WebApi;
+
+namespace Contest.Host.Controllers.Api
+{
+  [RoutePrefix("api/swagger")]
+  public class SwaggerApiController : ApiController
+  {
+    private static readonly Lazy<string> SwaggerContent = new Lazy<string>(() =>
+    {
+      var controllers = new[] {typeof(CustomerApiController)};
+      var settings = new WebApiToSwaggerGeneratorSettings
+      {
+        DefaultUrlTemplate = "api/{controller}/{action}/{id}"
+      };
+
+      var generator = new WebApiToSwaggerGenerator(settings);
+      var service = generator.GenerateForControllers(controllers);
+
+      service.Info = new SwaggerInfo
+      {
+        Title = "Demo WebApi swagger Service",
+        Description = "Demo for the Api-workshop",
+        TermsOfService = "See license",
+        License = new SwaggerLicense {Name = "Apache 2.0", Url = "http://www.apache.org/licenses/LICENSE-2.0"},
+        Version = "1.0.0",
+        Contact =
+          new SwaggerContact
+          {
+            Email = "mario.toffia@dataductus.se",
+            Name = "Mario Toffia",
+            Url = "http://www.dataductus.se"
+          }
+      };
+
+      service.Host = "localhost:53233";
+      service.BasePath = "/";
+      service.Schemes = new List<SwaggerSchema> {SwaggerSchema.Http};
+
+      return service.ToJson();
+    });
+
+    [HttpGet, Route("")]
+    public HttpResponseMessage Get()
+    {
+      var res = Request.CreateResponse(HttpStatusCode.OK);
+      res.Content = new StringContent(SwaggerContent.Value, Encoding.UTF8, "application/json");
+      return res;
+    }
+  }
+}
